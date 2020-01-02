84763327c3fe02b3371ee2a12a59af1dd4185311
“mohsenghazi” <59336771+mohsenghazi@users.noreply.github.com>
2020-01-02 11:41:55 +0330

    Change forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,6 @@
 const request = require('request')
 const chalk = require('chalk')
 
-
 const forecast = (latitude, longitude, callback) =>{
 
     const url = 'https://api.darksky.net/forecast/ed1451d639a720517e2c368dd2bd5bc4/'+ latitude +','+ longitude + '?units=si'
@@ -11,10 +10,9 @@ const forecast = (latitude, longitude, callback) =>{
          } else if(body.error){
             callback('Unable to find location', undefined)
          } else{
-             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out.There is a ' + body.currently.precipProbability + ' % chance of rain')
+             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + ' % chance of rain')
          }
      })
 }
-
 
 module.exports = forecast
